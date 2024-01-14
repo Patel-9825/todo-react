793b1a07a070b5dac4fc2010c2af53a0941cbece
Dhruv Patel <dhruvpatel@d-h-r-u-v-s-mbp.home>
2024-01-13 23:34:52 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return <>
+    <div className="main-div">
+      <div className="center-div">
+        <br />
+        <h1> ToDo List</h1>
+        <br />
+        <input />
+      </div>
+    </div>
+  </>;
+};
+
+export default App;
